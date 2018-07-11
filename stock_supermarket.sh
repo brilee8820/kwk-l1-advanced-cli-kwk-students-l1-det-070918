77d0f06f15a3447cc45189a2f600ae8521faeaad
brilee8820 <brilee8820@gmail.com>
2018-07-11 19:00:02 +0000

    automatically backed up by learn

---
+++ stock_supermarket.sh
@@ -0,0 +1,14 @@
+ls;
+mv delivery/bison_burgers.txt freezer;
+mv delivery/broccoli.txt veggies;
+mv delivery/brie.txt dairy;
+mv delivery/carrot_cake.txt dessert;
+mv delivery/carrots.txt veggies;
+mv delivery/eggo_waffles.txt freezer;
+mv delivery/fage_yogurt.txt dairy;
+mv delivery/fig_bar.txt snax;
+mv delivery/flourless_cake.txt dessert;
+mv delivery/garlic.txt veggie;
+mv delivery/goldfish.txt snax;
+mv delivery/grapples.txt fruit;
+
